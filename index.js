d3d3bd63e92f9228f869c7bbc78148eeb9ff0f8c
HilmaKorkeila <hilma.korkeila@student.hamk.fi>
2026-04-15 19:41:30 +0300

    Adding array of objects

--- index.js
+++ index.js
@@ -3,5 +3,48 @@ const path = require('path');
 
 const app = express();
 
+let clothes = [
+    {
+        id: 1,
+        name: dress,
+        price: 50,
+        brand: Mango,
+        size: S,
+        color: black,
+        inStock: true, 
+    },
+    {
+        id: 2,
+        name: blouse,
+        price: 20,
+        brand: H&M,
+        size: M,
+        color: white,
+        inStock: false, 
+    },
+    {
+        id: 3,
+        name: skirt,
+        price: 40,
+        brand: Cubus,
+        size: S,
+        color: black,
+        inStock: true, 
+    },
+    {
+        id: 4,
+        name: pants,
+        price: 90,
+        brand: Carlings,
+        size: M,
+        color: brown,
+        inStock: true, 
+    }
+]
+
+app.get('/api/products'), (req,res) => {
+    res.json(products);
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
